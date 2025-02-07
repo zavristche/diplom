04762085925f5b40d09e3f31292050264dc02c83
Kate Gromova <categrm@yandex.ru>
2025-02-07 20:27:02 +0300

    +docker

--- web/frontend/src/api/RecipeService.js
+++
@@ -1,9 +0,0 @@
-import EntityService from './EntityService';
-
-class RecipeService extends EntityService {
-    constructor() {
-        super('recipe'); // Указываем путь для рецептов
-    }
-}
-
-export default new RecipeService();
--- web/frontend/src/views/About.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <div>ЭТО ABOUT</div>
-</template>
-
-<script>
-
-
-</script>
