37bac1412c02f3e3c0bbdf8e5ab64e1d21998f2d
Kate Gromova <categrm@yandex.ru>
2025-02-11 23:16:00 +0300

    docker vue-app fix reactivity in processing

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,7 +1,25 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+const webpack = require('webpack');
+
 module.exports = defineConfig({
-  transpileDependencies: true,
   devServer: {
-    port: 5173
-  }
-})
+    host: '0.0.0.0', // Доступ из контейнера
+    port: 8282,
+    watchFiles: ['src/**/*'], // Следим за изменениями
+    hot: true, // Включаем HMR (Hot Module Replacement)
+    liveReload: true, // Принудительно перезапускать страницу при изменениях
+    client: {
+      overlay: true, // Показывать ошибки в браузере
+    },
+  },
+  configureWebpack: {
+    plugins: [
+      new webpack.DefinePlugin({
+        // Определяем feature flags для Vue
+        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: JSON.stringify(false), // Отключаем детали hydration mismatch
+        __VUE_OPTIONS_API__: JSON.stringify(true), // Включаем поддержку Options API
+        __VUE_PROD_DEVTOOLS__: JSON.stringify(false), // Отключаем Vue Devtools в production
+      }),
+    ],
+  },
+});
